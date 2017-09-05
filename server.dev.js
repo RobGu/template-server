03b39c21c0187f54113379a0e20a1b64e6810d50
rob.gu <rob.gu@augmentum.com>
2017-09-05 14:53:14 +0800

    package: update to the latest version

--- server.dev.js
+++ server.dev.js
@@ -1,10 +1,12 @@
 import Koa from 'koa';
+import Logger from 'koa-logger';
 
 import api from './server/middleware';
 import Static from './static/middleware';
 import Web from './web/middleware';
 
 const app = new Koa();
+app.use(Logger());
 app.use(Static());
 app.use(api.routes(), api.allowedMethods());
 app.use(Web());
